dcd54df1255b1acd63e0bb64b9c02ee2eb6788b1
FastFerrous <fastferrous@example.com>
2026-05-17 16:46:31 -0400

    just finished the enqueue portion, once done with all of them, need to merge back to template basically

--- sozo/modules/file_ops/src/lib.rs
+++ sozo/modules/file_ops/src/lib.rs
@@ -5,16 +5,17 @@ use tokio::sync::Mutex;
 use tokio_util::sync::CancellationToken;
 
 struct FileOperations {
-    rx: Mutex<Receiver<BusMessage>>,
-    tx: Sender<BusMessage>,
+    rx: Mutex<Receiver<Vec<u8>>>,
+    tx: Sender<Vec<u8>>,
 }
 
 impl FileOperations {
-    fn new() -> Self {
+    fn new() -> FileOperations {
         const MAX_MSG_BUFFER: usize = 1024;
 
         let (tx, rx) = tokio::sync::mpsc::channel(MAX_MSG_BUFFER);
-        Self {
+
+        FileOperations {
             rx: Mutex::new(rx),
             tx,
         }
@@ -32,7 +33,22 @@ unsafe extern "C" fn plugin_destroy(instance: *mut c_void) {
 unsafe extern "C" fn plugin_run(instance: *mut c_void, channel: *mut c_void, token: *mut c_void) {}
 
 unsafe extern "C" fn plugin_enqueue(instance: *mut c_void, msg: *const u8, len: usize) -> u8 {
-    0u8
+    if instance.is_null() || msg.is_null() {
+        return false as u8;
+    }
+
+    let slice_msg = unsafe { std::slice::from_raw_parts(msg, len)};
+
+    let mut msg : Vec<u8> = Vec::new();
+    if msg.try_reserve(slice_msg.len()).is_err(){
+        return false as u8;
+    }
+
+    msg.extend_from_slice(slice_msg);
+
+    unsafe {
+        (&*(instance as *const FileOperations)).tx.try_send(msg).is_ok() as u8
+    }
 }
 
 static VTABLE: ModuleVTable = ModuleVTable {
@@ -46,9 +62,6 @@ static VTABLE: ModuleVTable = ModuleVTable {
 pub unsafe extern "C" fn module_entry() -> *const ModuleVTable {
     &VTABLE
 }
-
-// finish up the main extern functions and then work on internals parsing of certain things. just do simple prints for now.
-// once built, just make the loader and see if we can do that
 
 /*
  * unsafe extern "C" fn run(state: *mut c_void, sender: *mut c_void, token: *mut c_void) {
